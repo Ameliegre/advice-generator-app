7a471e15f98afce14de08789080bc3139872591b
Amélie Grenier <amelie.grenier@hotmail.fr>
2023-05-31 12:28:29 +0200

    add design + call api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import patternD from './images/pattern-divider-desktop.svg'
+import iconBtn from './images/icon-dice.svg'
+import axios from 'axios'
+import { useState } from 'react'
+
 function App() {
+
+  const [quote , setQuote] = useState('')
+
+  const getQuote = async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      console.log(response.data.slip);
+      setQuote(response.data.slip)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  const handleClick = () => {
+    getQuote()
+  }
+
+  
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-    <p className="text-3xl text-gray-700 font-bold mb-5">
-      Welcome!
-    </p>
-    <p className="text-gray-500 text-lg">
-      React and Tailwind CSS in action
-    </p>
+    <div className="container mx-auto w-2/5 h-60 px-6 bg-dark-grayish-blue rounded-xl shadow border p-4 m-12 
+      flex flex-col items-center relative">
+      <p className="uppercase text-neon-green text-xs mt-6">
+        advice #{quote.id}
+      </p>
+      <p className="text-xl font-extrabold text-light-cyan mt-6">
+        "{quote.advice}"
+      </p>
+      <img src={patternD} alt='pattern' className='mt-6'/>
+      <button type="submit" onClick={handleClick} className='bg-neon-green p-3 rounded-full cursor-pointer absolute bottom-0 hover:shadow-2xl hover:bg-red-600 hover:ease-in-out'>
+        <img src={iconBtn} alt='icon' className='h-4'/>
+      </button>
   </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'light-cyan': 'hsl(193, 38%, 86%)',
+      'neon-green': 'hsl(150, 100%, 66%)',
+      'grayish-blue': 'hsl(217, 19%, 38%)',
+      'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+      'dark-blue': 'hsl(218, 23%, 16%)'
+    },
+    extend: {}
   },
-  plugins: [],
-}
-
+  plugins: []
+};
